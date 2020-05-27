eb2a7409d49571dc15ef26676f6ea21aee45a999
maslana146@gmail.com <maslana146@gmail.com>
2020-05-27 20:42:19 +0200

    Search function, items

--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-
-
+from bs4 import BeautifulSoup
+import requests
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search': search,
+
+    }
+    return render(request, 'my_app/new_search.html',stuff_for_frontend)
